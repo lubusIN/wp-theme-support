fd572db3d2dbbde5a385b867b3a57d37aec50b81
Ajit Bohra <ajit@lubus.in>
2018-11-19 15:27:20 +0530

    Update width labels

--- src/components/preferences/widths.js
+++ src/components/preferences/widths.js
@@ -19,26 +19,24 @@ const Widths = () => {
     <PreferencesContext.Consumer>
       {context => (
         <PanelBody title="Block Editor Width" initialOpen={false}>
-          <PanelRow className="wp-editor-preferences-sidebar__fontsize">
-            <TextControl
-              label="Main"
-              value={context.preferences.widths.main}
-              className="wp-editor-preferences-sidebar__width-main"
-              onChange={value => update(context, "main", value)}
-            />
-            <TextControl
-              label="Wide"
-              value={context.preferences.widths.wide}
-              className="wp-editor-preferences-sidebar__width-wide"
-              onChange={value => update(context, "wide", value)}
-            />
-            <TextControl
-              label="Full"
-              value={context.preferences.widths.full}
-              className="wp-editor-preferences-sidebar__width-full"
-              onChange={value => update(context, "full", value)}
-            />
-          </PanelRow>
+          <TextControl
+            label="Main Column"
+            value={context.preferences.widths.main}
+            className="wp-editor-preferences-sidebar__width-main"
+            onChange={value => update(context, "main", value)}
+          />
+          <TextControl
+            label="Wide Blocks"
+            value={context.preferences.widths.wide}
+            className="wp-editor-preferences-sidebar__width-wide"
+            onChange={value => update(context, "wide", value)}
+          />
+          <TextControl
+            label="Full Wide Blocks"
+            value={context.preferences.widths.full}
+            className="wp-editor-preferences-sidebar__width-full"
+            onChange={value => update(context, "full", value)}
+          />
         </PanelBody>
       )}
     </PreferencesContext.Consumer>
